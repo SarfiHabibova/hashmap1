246cc6a062edf01d0f4a52271605d08a8a0d8595
teslagist <130398550+teslagist@users.noreply.github.com>
2023-12-18 15:52:02 +0400

    hey
    salam2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
         System.out.println("E-library");
         String love = "Cavidan love Sarfi very big";
         int u=6;
         int cavidan = 123123;
         System.out.println("artiq deyis amk cocugu");
->>>>>>> Stashed changes
+        System.out.println("artiq deyis amk cocugu2");
+
         }
     }
